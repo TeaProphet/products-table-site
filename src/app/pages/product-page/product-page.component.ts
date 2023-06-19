03c909ae42d8959b86fac16e698c90e441e03f55
TeaProphet <0teaprophet0@gmail.com>
2023-06-19 21:20:16 +0300

    Updated page titles

--- src/app/pages/product-page/product-page.component.ts
+++ src/app/pages/product-page/product-page.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ProductsService} from "../../services/products.service";
 import {ActivatedRoute} from "@angular/router";
+import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-product-page',
@@ -8,13 +9,16 @@ import {ActivatedRoute} from "@angular/router";
   styleUrls: ['./product-page.component.css']
 })
 export class ProductPageComponent implements OnInit{
+  title = 'Товар';
   loading: boolean = true
 
-  constructor(private route: ActivatedRoute, public productsService: ProductsService) {}
+  constructor(private route: ActivatedRoute, public productsService: ProductsService, private titleService: Title) {}
 
   ngOnInit() {
+    this.titleService.setTitle(this.title)
     this.route.params.subscribe(params => {
-      this.productsService.get(params['id']).subscribe(() => this.loading = false)
+      this.productsService.get(params['id']).subscribe(() =>
+      {this.loading = false})
     });
   }
 }
--- src/app/pages/products-table-page/products-table-page.component.ts
+++ src/app/pages/products-table-page/products-table-page.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Observable, tap} from "rxjs";
 import {IProduct} from "../../models/product";
 import {ProductsService} from "../../services/products.service";
+import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-products-table-page',
@@ -9,13 +10,14 @@ import {ProductsService} from "../../services/products.service";
   styleUrls: ['./products-table-page.component.css']
 })
 export class ProductsTablePageComponent implements OnInit {
-  title = 'таблица товаров';
+  title = 'Таблица Товаров';
   loading: boolean = true
 
-  constructor(public productsService: ProductsService) {
+  constructor(public productsService: ProductsService, private titleService: Title) {
   }
 
   ngOnInit(): void {
+    this.titleService.setTitle(this.title)
     this.productsService.getAll().subscribe(() => this.loading = false)
   }
 }
